629fb2b12710a70272751ff1f28f6af7b50c72e0
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-07-14 10:36:19 +0500

    jj

--- lab10/cart.php
+++ lab10/cart.php
@@ -5,14 +5,11 @@ require_once 'libs/Smarty.class.php';
 
 $smarty = new Smarty();
 
-$total_cost = 0;
-
-$smarty->assign('dbh', $dbh);
-
 if (isset($_COOKIE['cart']) && !empty($_COOKIE['cart'])) {
 	$smarty->assign('cart_cookie', $_COOKIE['cart']);
 }
 
-$smarty->assign('total_cost', $total_cost);
+$smarty->assign('dbh', $dbh);
+$smarty->assign('total_cost', 0);
 
 $smarty->display('cart.tpl');
